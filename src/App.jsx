fcb70fac47885efac9292c7329682fb75f1c2ebe
Fidel D. Oliveira <fideldeoliveira08@gmail.com>
2026-09-04 09:05:12 +0100

    feat: Routas adicionadaas e criando comoponentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <p>manegerApp</p>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
