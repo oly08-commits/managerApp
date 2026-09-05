a8158a71d0df6737e5e221a8f64771c4812e5ded
Fidel D. Oliveira <fideldeoliveira08@gmail.com>
2026-09-05 04:33:08 +0100

    feat: Navbar concluida, por enquanto!

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,29 @@
+import Container from "./Container";
+
+import styles from "./Navbar.module.css";
+
+import logo from "../../img/costs_logo.png";
+
 function Navbar({ Link }) {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contato</Link>
-      <Link to="/newProject">Novo Projeto</Link>
-      <Link to="/about">Sobre</Link>
-    </div>
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="ícone do managerApp" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Ínicio</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contacto</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/about">Sobre</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
   );
 }
 
