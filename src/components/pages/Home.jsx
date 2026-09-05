fcc00df686026e8caeb0105c45ce7331099ff689
Fidel D. Oliveira <fideldeoliveira08@gmail.com>
2026-09-05 06:46:59 +0100

    feat: home concluido

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,18 @@
+import styles from "./Home.module.css";
+
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
   return (
-    <div>
-      <h1>Ínicio</h1>
-    </div>
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo ao <span>managerApp</span>
+      </h1>
+      <p>Comece a gerenciar os seus projecto aqui, não perca mais tempo!</p>
+      <LinkButton to="/newProject" text="Criar Projecto" />
+      <img src={savings} alt="managerApp" />
+    </section>
   );
 }
 
